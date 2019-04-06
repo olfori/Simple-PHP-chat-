7a28bca740c4665c9393350d977fddaf3a4bd0ce
olfori <olfori@yandex.ru>
2019-04-06 19:21:51 +0300

    Update index.php

--- index.php
+++ index.php
@@ -35,7 +35,7 @@
 <script src="https://code.jquery.com/ui/1.12.0/jquery-ui.min.js" integrity="sha256-eGE6blurk5sHj+rmkfsGYeKyZx3M4bG+ZlFyA7Kns7E="
 crossorigin="anonymous"></script>
 
-<script src="/js/Chat.js"></script>
+<script src="Chat.js"></script>
 
 </body>
 </html>
